cde0dc98ed310254a20571cf3cde791efbce7c5d
Saman Hosseini <bounoable@gmail.com>
2019-11-05 16:30:36 +0100

    make dep external

--- packages/plugins/selection/bili.config.ts
+++ packages/plugins/selection/bili.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
     }
   },
 
-  externals: ['lodash', 'uuid', 'date-fns', 'vue', '@vue/composition-api', 'resize-observer-polyfill'],
+  externals: ['lodash', 'uuid', 'date-fns', 'vue', '@vue/composition-api', 'resize-observer-polyfill', 'vue-airbnb-calendar'],
 
   env: {
     NODE_ENV: 'production',
